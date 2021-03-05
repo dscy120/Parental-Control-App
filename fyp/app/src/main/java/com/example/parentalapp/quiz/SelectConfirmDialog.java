b494a68d12dc9011b254fe9b6a31f9df9e4f070e
dscy120 <dscytp@gmail.com>
2021-03-05 14:04:12 +0800

    Update question database

--- fyp/app/src/main/java/com/example/parentalapp/quiz/SelectConfirmDialog.java
+++ fyp/app/src/main/java/com/example/parentalapp/quiz/SelectConfirmDialog.java
@@ -8,25 +8,39 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.example.parentalapp.R;
+import com.example.parentalapp.admin.questionbank.QuestionBankDBHelper;
+import com.example.parentalapp.quiz.question.Question;
+import com.example.parentalapp.quiz.question.QuizDBHelper;
+
+import java.util.List;
 
 import static com.example.parentalapp.quiz.QuizSelectionActivity.SUBJECT;
 import static com.example.parentalapp.quiz.QuizSelectionActivity.DIFFICULTY;
 
 public class SelectConfirmDialog extends AppCompatDialogFragment {
     private SelectConfirmDialogListener listener;
+    private QuizDBHelper quizDBHelper;
+    private String subject, difficulty;
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         final View customLayout = getActivity().getLayoutInflater().inflate(R.layout.dialog_confirm_quiz_detail, null);
 
-        setDialogInfo(customLayout, getArguments().getString(SUBJECT), getArguments().getString(DIFFICULTY));
+        subject = getArguments().getString(SUBJECT);
+        difficulty = getArguments().getString(DIFFICULTY);
+
+        quizDBHelper = new QuizDBHelper(getContext());
+        final List<Question> questions = quizDBHelper.getQuestions(subject, difficulty);
+
+        setDialogInfo(customLayout, subject, difficulty);
 
         builder.setView(customLayout);
         builder.setTitle("Confirm Quiz Details")
@@ -39,7 +53,11 @@ public class SelectConfirmDialog extends AppCompatDialogFragment {
                 .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        listener.startQuiz();
+                        if(questions.size() <= 0){
+                            Toast.makeText(getContext(), "There is no questions available.", Toast.LENGTH_SHORT).show();
+                        }else{
+                            listener.startQuiz();
+                        }
                     }
                 });
         return builder.create();
